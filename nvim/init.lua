426ba8acd90a422e361ec26e80f6542eac897258
MysticalRobot <dhruv21420@gmail.com>
2025-08-15 16:29:35 -0400

    switched to blimp.cmp from native autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,36 +24,53 @@ vim.cmd(':hi statusline guibg=NONE') -- remove the background the status line
 
 -- [[ plugins ]]
 vim.pack.add({
-	-- directory of language server quickstart configurations
-	{ src = 'https://github.com/neovim/nvim-lspconfig' },
-	-- theme
-	{ src = 'https://github.com/ellisonleao/gruvbox.nvim' },
-	-- debugger
-	{ src = 'https://github.com/mfussenegger/nvim-dap' },
-	-- displays the actions that are possible from further key presses
-	{ src = 'https://github.com/folke/which-key.nvim' },
-	-- live markdown previewer
-	{ src = 'https://github.com/brianhuster/live-preview.nvim' },
-	-- handle surrounding text better (brackets, quotes, etc.)
-	-- sa = add surrounding, sd = delete surrounding,
-	-- sh = highlight surrounding, sr = replace surrounding,
-	-- sf = find right surrounding, sF = find left surrounding
-	{ src = 'https://github.com/echasnovski/mini.surround' },
-	-- file picker
-	{ src = 'https://github.com/nvim-telescope/telescope.nvim' },
-	-- goated terminal file explorer along with its dependencies
-	{ src = 'https://github.com/mikavilpas/yazi.nvim' },
-	{ src = 'https://github.com/nvim-lua/plenary.nvim' },
-	{ src = 'https://github.com/folke/snacks.nvim' },
+  -- directory of language server quickstart configurations
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  -- autocompletion
+  { src = 'https://github.com/saghen/blink.cmp' },
+  -- theme
+  { src = 'https://github.com/ellisonleao/gruvbox.nvim' },
+  -- debugger
+  { src = 'https://github.com/mfussenegger/nvim-dap' },
+  -- displays the actions that are possible from further key presses
+  { src = 'https://github.com/folke/which-key.nvim' },
+  -- live markdown previewer
+  { src = 'https://github.com/brianhuster/live-preview.nvim' },
+  -- handle surrounding text better (brackets, quotes, etc.)
+  -- sa = add surrounding, sd = delete surrounding,
+  -- sh = highlight surrounding, sr = replace surrounding,
+  -- sf = find right surrounding, sF = find left surrounding
+  { src = 'https://github.com/echasnovski/mini.surround' },
+  -- file picker
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+  -- goated terminal file explorer along with its dependencies
+  { src = 'https://github.com/mikavilpas/yazi.nvim' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/folke/snacks.nvim' },
 })
-
 -- plugin configurations
+require('blink.cmp').setup({
+  fuzzy = {
+    implementation = 'prefer_rust',
+  },
+  completion = {
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    ghost_text = { enabled = true },
+    menu = {
+      auto_show = true,
+      draw = {
+        treesitter = { 'lsp' },
+        columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } }
+      }
+    }
+  },
+  signature = { enabled = true }
+})
 require('gruvbox').setup({ contrast = 'hard' })
 vim.cmd('colorscheme gruvbox')
 require('which-key').setup({ preset = 'helix' })
 require('mini.surround').setup()
--- replace netrw, nvim's default file explorer
-require('yazi').setup({ open_for_directories = true })
+require('yazi').setup()
 
 -- [[ keymaps ]]
 -- plugin mappings
@@ -64,10 +81,10 @@ vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { desc = 'search rece
 vim.keymap.set('n', '<leader>h', ':Telescope help_tags<CR>', { desc = 'search help' })
 -- disable s so that mini.surround can use s
 vim.api.nvim_set_keymap('n', 's', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 's', '<Nop>', { noremap = true, silent = true })
 -- lsp mappings
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'open diagnostic quickfix list' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'open diagnostic' })
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'format file' })
 vim.keymap.set('n', '<leader>i', vim.lsp.buf.signature_help, { desc = 'inspect signature' })
 -- centered screen scrolling
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'scroll half page down and center' })
@@ -86,21 +103,27 @@ vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { silent = true, noremap = true })
-
 -- highlight copied text
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	callback = function()
-		vim.hl.on_yank()
-	end,
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+-- format buffer (file) on write
+vim.api.nvim_create_autocmd('BufWrite', {
+  desc = 'format buffer on write',
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
 -- intuitive navigation between panes of both tmux and nvim
 local function smart_move(direction, tmux_cmd)
-	local curwin = vim.api.nvim_get_current_win()
-	vim.cmd('wincmd ' .. direction)
-	if curwin == vim.api.nvim_get_current_win() then
-		vim.fn.system('tmux select-pane ' .. tmux_cmd)
-	end
+  local curwin = vim.api.nvim_get_current_win()
+  vim.cmd('wincmd ' .. direction)
+  if curwin == vim.api.nvim_get_current_win() then
+    vim.fn.system('tmux select-pane ' .. tmux_cmd)
+  end
 end
 vim.keymap.set('n', '<C-h>', function() smart_move('h', '-L') end, { silent = true })
 vim.keymap.set('n', '<C-j>', function() smart_move('j', '-D') end, { silent = true })
@@ -108,16 +131,6 @@ vim.keymap.set('n', '<C-k>', function() smart_move('k', '-U') end, { silent = tr
 vim.keymap.set('n', '<C-l>', function() smart_move('l', '-R') end, { silent = true })
 
 -- [[ language server provider (lsp) setup ]]
--- enable autocomplete (use it with <C-X> and <C-O>)
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
-})
-vim.cmd('set completeopt+=noselect') -- fixes some autocomplete annoyance
--- enable lsps (to add more, download the lsp, add it here, configure it
+-- (to add more, download the lsp, add it here, and configure it
 -- like the other language servers in ./lsp/, with :help lspconfig-all)
 vim.lsp.enable({ 'clangd', 'lua_ls', 'ts_ls', 'pyright', 'racket_langserver' })
